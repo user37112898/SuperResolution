e7c9db4af683ad691faf233a850bf0bf4e11f8ec
yashthakkar1173 <yashthakkar1173@gmail.com>
2020-04-11 00:34:27 +0530

    Added Loader and Image Description

--- WebApplication/main.js
+++ WebApplication/main.js
@@ -1,33 +1,44 @@
 const inputFile = document.getElementById('inputFile');
 const previewContainer = document.getElementById('imagePreview');
 const previewImage = previewContainer.querySelector('.image-preview_image');
-// const previewDefaultText = previewContainer.querySelector('.image-preview_default-text');
 const imageSection = document.getElementById('imageSection');
-
+const loaderImg = document.getElementById('loading-image');
+const loaderDiv = document.getElementById('loading');
+const imageDescription = document.getElementById('imageDescription');
 const reader = new FileReader();
 var imageObject = new String();
 var fileName = new String();
+var img = new Image;
+
 function openFileFunction(){
 	inputFile.click();
-	imageSection.style.display = 'block'
+	console.log($("#imageSection").offset().top);
 }
+
 inputFile.addEventListener("change",function(){
 	const file = this.files[0];
+	imageDescription.innerHTML = "Low Resolution Image"
 	
 	if(file){
-		
-		// previewDefaultText.style.display= 'none';
 		previewImage.style.display= 'block';
+		img.onload = function() {
+			loaderImg.height = previewImage.clientHeight;
+			console.log(this.naturalHeight)
+			console.log(previewImage.naturalHeight)
+		}
+		img.src = URL.createObjectURL(file);
 		
 		reader.addEventListener("load",function(){
-			console.log(this);
+			imageSection.style.display = 'block'
+			$('html, body').animate({
+				scrollTop: $('#imageSection').offset().top - 80 
+			}, 1000);
 			var str = reader.result;
 			var strA = str.split(',')
 			imageObject = strA[strA.length-1]
 			fileName = file.name
 			previewImage.setAttribute('src',this.result);
 		});
-		
 		reader.readAsDataURL(file);
 	}else{
 		previewDefaultText.style.display= 'null';
@@ -38,9 +49,12 @@ inputFile.addEventListener("change",function(){
 });
 
 function performFunction() {
+	loaderDiv.style.display = 'block'
 	eel.superFunction(imageObject,fileName)(setImage);
 }
 
 function setImage(base64) {
 	previewImage.setAttribute('src',base64);
+	loaderDiv.style.display = 'none'
+	imageDescription.innerHTML ="High Resolution Image"
 }
